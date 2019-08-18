e2032ff0779c8a98e34a647ab0734a96712e16a7
Beth Smith <bethsmith0623@gamil.com>
2019-08-17 22:59:35 -0500

    adds create router

--- routes/skills.js
+++ routes/skills.js
@@ -6,7 +6,7 @@ var skillsCtrl = require('../controllers/skills');
 router.get('/', skillsCtrl.index);
 router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
-
+router.post('/', skillsCtrl.create);
 
 
 module.exports = router;
