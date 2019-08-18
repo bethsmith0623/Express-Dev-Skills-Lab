fa2e927f36e3d9e3c128eb3643d44a95b0e7923b
Beth Smith <bethsmith0623@gamil.com>
2019-08-17 21:28:46 -0500

    adds last updated date

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,8 +2,15 @@ var Skill = require('../models/skill');
 
 module.exports = {
   index, 
+  show
 };
 
+function show(req, res){
+  res.render('skills/show', {
+    skill: Skill.getOne(req.params.id),
+    skillNum: parseInt(req.params.id) + 1
+  });
+}
 
 function index(req, res){
   res.render('skills/index', {
