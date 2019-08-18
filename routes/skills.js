8c3273ec9b222c07212735405dd019382db96576
Beth Smith <bethsmith0623@gamil.com>
2019-08-17 23:38:41 -0500

    adds delete router

--- routes/skills.js
+++ routes/skills.js
@@ -7,6 +7,7 @@ router.get('/', skillsCtrl.index);
 router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
 router.post('/', skillsCtrl.create);
+router.delete('/:id', skillsCtrl.delete);
 
 
 module.exports = router;
