43da7b595611134b57cdba08e8561e3e83dfdbaf
Beth Smith <bethsmith0623@gamil.com>
2019-08-17 22:18:34 -0500

    removes console.log

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(function(req,res,next){
-  console.log("Hello SEI!");
   req.date = new Date().toLocaleDateString();
   next();
 })
