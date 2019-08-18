f7dbef2593a1bb836ce3d332aba0b376015874d3
Beth Smith <bethsmith0623@gamil.com>
2019-08-17 21:49:51 -0500

    updates data

--- data/skills.js
+++ data/skills.js
@@ -1,17 +1,18 @@
 const skills = [
-  {skill: 'HTML', level: '0'},
-  {skill: 'CSS', level: '0'},
-  {skill: 'CSS Bootstrap', level: '0'},
-  {skill: 'JavaScript', level: '0'},
-  {skill: 'MongoDB', level: '0'},
-  {skill: 'Mongoose', level: '0'},
-  {skill: 'Express.js', level: '0'},
-  {skill: 'React', level: '0'},
-  {skill: 'Node.js', level: '0'},
-  {skill: 'Python', level: '0'},
-  {skill: 'Django', level: '0'},
-  {skill: 'PostgreSQL', level: '0'},
+  {skill: 'HTML', level: '3', description: 'I have adequate proficiently using this skill in a variety of applications.'},
+  {skill: 'CSS', level: '3', description: 'I have adequate proficiently using this skill in a variety of applications.'},
+  {skill: 'CSS Bootstrap', level: '1', description: 'I have limited experience with this skill at this point.'},
+  {skill: 'JavaScript', level: '3', description: 'I have adequate proficiently using this skill in a variety of applications.'},
+  {skill: 'MongoDB', level: '1', description: 'I have limited experience with this skill at this point.'},
+  {skill: 'Mongoose', level: '2', description: 'I have moderate experience with this skill.'},
+  {skill: 'Express.js', level: '2', description: 'I have moderate experience with this skill.'},
+  {skill: 'React', level: '0', description: 'I have not begun my studies of this skill yet.'},
+  {skill: 'Node.js', level: '1', description: 'I have limited experience with this skill at this point.'},
+  {skill: 'Python', level: '0', description: 'I have not begun my studies of this skill yet.'},
+  {skill: 'Django', level: '0', description: 'I have not begun my studies of this skill yet.'},
+  {skill: 'PostgreSQL', level: '0', description: 'I have not begun my studies of this skill yet.'},
 ];
+
 
 module.exports = {
   getAll
